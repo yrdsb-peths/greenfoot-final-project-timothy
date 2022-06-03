e31b562849c510b3bdebe74bcbba40d5251028b5
TimothyAlfred <101424271+TimothyAlfred@users.noreply.github.com>
2022-06-03 14:01:04 -0400

    Person can eat apples

--- finalproject-copy-copy/Apple.java
+++ finalproject-copy-copy/Apple.java
@@ -1,20 +1,19 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class snake here.
+ * Write a description of class Apple here.
  * 
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class snake extends Actor
+public class Apple extends Actor
 {
     /**
-     * Act - do whatever the snake wants to do. This method is called whenever
+     * Act - do whatever the Apple wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
-    
     public void act()
     {
-        
+        // Add your action code here.
     }
 }
--- finalproject-copy-copy/Person.java
+++ finalproject-copy-copy/Person.java
@@ -12,10 +12,29 @@ public class Person extends Actor
      * Act - do whatever the Person wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    GreenfootImage[] images;
+    public Person()
+    {
+        images = new GreenfootImage[8];
+        for(int i = 0; i < images.length; i++){
+            images[i] = new GreenfootImage("images/person_idle/idle" + i + ".png");
+        }
+    }
+    
     public void act()
     {
-        if(Greenfoot.isKeyDown("d")){
+        
+        if(Greenfoot.isKeyDown("w")){
             move(1);
         }
+        
+        if(Greenfoot.isKeyDown("space")){
+            turn(1);
+        }
+        
+        if(isTouching(Apple.class)){
+            removeTouching(Apple.class);
+        }
+            
     }
 }
